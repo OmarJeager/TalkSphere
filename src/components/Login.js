85d10903e9684aa5220b3bcd9e6fb3c78dffd323
Omar Jeager <stirnermax45@gmail.com>
2025-03-11 12:42:16 +0000

    update login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,9 @@
-// src/components/Login.js
 import React, { useState } from 'react';
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import { useNavigate, useLocation } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope, faLock } from '@fortawesome/free-solid-svg-icons';
+import './Login.css'; // Import the CSS file for styling
 
 const Login = () => {
   const [email, setEmail] = useState('');
@@ -27,22 +29,28 @@ const Login = () => {
   };
 
   return (
-    <div>
-      {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
-      <form onSubmit={handleLogin}>
-        <input
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button type="submit">Login</button>
+    <div className="login-container">
+      {errorMessage && <p className="error-message">{errorMessage}</p>}
+      <form onSubmit={handleLogin} className="login-form">
+        <div className="input-group">
+          <FontAwesomeIcon icon={faEnvelope} />
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="input-group">
+          <FontAwesomeIcon icon={faLock} />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button type="submit" className="login-button">Login</button>
       </form>
     </div>
   );
